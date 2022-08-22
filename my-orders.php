6e8c2045e7cdd2a356bafd23c35ebca551a6490b
duaneywadey <idrdequito.cvt@eac.edu.ph>
2022-08-22 17:01:23 +0800

    added orders table

--- my-orders.php
+++ my-orders.php
@@ -24,34 +24,45 @@ include('authenticate.php');
 <div class="py-5">
 	<div class="container">
 		<div class="row">
-			<?php $orders = getOrders(); 
-			if(mysqli_num_rows($orders) > 0) {
-				foreach ($orders as $item) {
-			?>
+			<div class="col-md-12">
+				<table class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Tracking No</th>
+							<th>Price</th>
+							<th>Date</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php $orders = getOrders(); 
+						if(mysqli_num_rows($orders) > 0) {
+							foreach ($orders as $item) {
+						?>
 
-			<div class="col-md-3 mb-4">
-				<div class="card">
-					<div class="card-body">
-						<img src="https://w7.pngwing.com/pngs/311/42/png-transparent-freight-transport-computer-icons-car-ship-shipping-angle-logo-cargo.png" width="250" height="250" alt="">
-						<p><?=$item['id']?></p>
-						<p><?=$item['tracking_no']?></p>
-						<p><?=$item['total_price']?></p>
-						<p><?=$item['created_at']?></p>
-					</div>
-				</div>
+						<tr>
+							<td><?= $item['id'] ?></td>
+							<td><?= $item['tracking_no'] ?></td>
+							<td><?= $item['total_price'] ?></td>
+							<td><?= $item['created_at'] ?></td>
+							<td><a href="#" class="btn btn-primary">View Details</a></td>	
+						</tr>
+
+						<?php 
+
+							} 
+
+						} 
+
+						else 
+						{
+							echo "<p>No data available</p>";
+						}
+						?>
+
+					</tbody>
+				</table>
 			</div>
-			<?php 
-
-				} 
-
-			} 
-
-			else 
-			{
-				echo "<p>No data available</p>";
-			}
-			?>
-
 		</div>
 	</div>
 </div>
